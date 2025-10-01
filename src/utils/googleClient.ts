b16b6e9a2c87f89b97a608f2389a654bb1b15acd
Krishna-synt <116875316+krishnakumars59809@users.noreply.github.com>
2025-10-01 15:53:59 +0530

    For testing

--- src/utils/googleClient.ts
+++ src/utils/googleClient.ts
@@ -12,29 +12,29 @@ const secretClient = new SecretManagerServiceClient({
   projectId: process.env.GOOGLE_PROJECT_ID,
 });
 
-async function getServiceAccountFromSecret(secretName: string) {
-  try {
-    const [version] = await secretClient.accessSecretVersion({
-      name: secretName,
-    });
+// async function getServiceAccountFromSecret(secretName: string) {
+//   try {
+//     const [version] = await secretClient.accessSecretVersion({
+//       name: secretName,
+//     });
 
-    if (!version.payload?.data) throw new Error("Secret payload is empty");
+//     if (!version.payload?.data) throw new Error("Secret payload is empty");
 
-    const creds = JSON.parse(version.payload.data.toString());
+//     const creds = JSON.parse(version.payload.data.toString());
 
-    // Fix private key formatting
-    creds.private_key = creds.private_key.replace(/\\n/g, "\n").trim();
+//     // Fix private key formatting
+//     creds.private_key = creds.private_key.replace(/\\n/g, "\n").trim();
 
-    console.log("✅ Secret fetched successfully");
-    return creds;
-  } catch (err: any) {
-    console.error(
-      "❌ Failed to fetch secret or invalid key:",
-      err.message || err
-    );
-    throw err;
-  }
-}
+//     console.log("✅ Secret fetched successfully");
+//     return creds;
+//   } catch (err: any) {
+//     console.error(
+//       "❌ Failed to fetch secret or invalid key:",
+//       err.message || err
+//     );
+//     throw err;
+//   }
+// }
 
 export async function createSpeechClient() {
   const project_id = process.env.GOOGLE_PROJECT_ID;
@@ -46,16 +46,19 @@ export async function createSpeechClient() {
     throw new Error(
       "Missing GOOGLE_PROJECT_SECRETS_VERSION in environment variables"
     );
-  const secretCreds = await getServiceAccountFromSecret(
-    `projects/${project_id}/secrets/familyNation/versions/${project_secrets_version}`
-  );
+  // const secretCreds = await getServiceAccountFromSecret(
+  //   `projects/${project_id}/secrets/familyNation/versions/${project_secrets_version}`
+  // );
 
   const client = new SpeechClient({
-    credentials: {
-      client_email: secretCreds.client_email,
-      private_key: secretCreds.private_key,
-    },
-    projectId: secretCreds.project_id,
+      credentials: {
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n"
+    ),
+  },
+  projectId: process.env.GOOGLE_PROJECT_ID,
   });
 
   console.log("✅ Speech client created successfully");
